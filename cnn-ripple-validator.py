12740a5fc9e7fdb4ddaa975e1f50dc362040edb1
Pavel Kulik <pavelkulik@hotmail.com>
2023-04-24 13:57:17 -0700

    Load data w/ missing continuous

--- cnn-ripple-validator.py
+++ cnn-ripple-validator.py
@@ -18,10 +18,17 @@ pd.set_option('display.max_rows', 40)
 duration_threshold = 100
 
 #Define base path to data
-base_path = 'E:/PridaLabData/data_recieved_3_31_2023/2023-01-27_13-19-21_Threshold0_9/Record Node 115'
+base_path = 'E:/PridaLabData/data_recieved_3_31_2023'
+data_sets = [
+    os.path.join(base_path, '2023-01-27_13-19-21_Threshold0_9/Record Node 115'),
+    os.path.join(base_path, '2023-01-27_13-29-15_Threshold0_9/Record Node 115'),
+    os.path.join(base_path, '2023-01-27_13-11-25_Threshold0_8/Record Node 115')
+]
+
+data_path = data_sets[0]
 
 #List all file at base path
-file_list = os.listdir(base_path)
+file_list = os.listdir(data_path)
 
 continuous = {}
 
@@ -33,7 +40,7 @@ fmt_str = '{:<40} {:>18} {:>40}'
 fmt_str_len = 40 + 18 + 40 + 2  # 2 spaces between each column ?
 
 for file_name in file_list:
-    file_size = os.path.getsize(os.path.join(base_path, file_name))
+    file_size = os.path.getsize(os.path.join(data_path, file_name))
     extension = os.path.splitext(file_name)[1]
     #print(fmt_str.format(file_name, extension, file_size))
     if extension == '.continuous':
@@ -51,7 +58,7 @@ print('-' * fmt_str_len)
 #print(fmt_str.format('Total size of continuous files ', '', sum(continuous.values())))
 
 #Get size of .dat file
-dat_file = os.path.join(base_path, 'ephys.dat')
+dat_file = os.path.join(data_path, 'ephys.dat')
 
 #Print size of .dat file
 #print(fmt_str.format('Size of .dat file ', '', os.path.getsize(dat_file)))
@@ -59,9 +66,10 @@ dat_file = os.path.join(base_path, 'ephys.dat')
 #Missing bytes
 #print(fmt_str.format('Non data bytes ', '',  sum(continuous.values()) - os.path.getsize(dat_file)))
 
-num_channels = len(continuous.keys())
+num_channels = len(continuous.keys()) if len(continuous.keys()) > 0 else 43
+
 #Load .dat file
-data = np.memmap(os.path.join(base_path, 'ephys.dat'), mode='r', dtype='int16')
+data = np.memmap(os.path.join(data_path, 'ephys.dat'), mode='r', dtype='int16')
 samples = data.reshape((len(data) // num_channels, num_channels))
 
 samples_per_chan = int(samples.size/num_channels)
@@ -79,41 +87,49 @@ samples_per_chan = int(samples.size/num_channels)
 #Start a timer
 start_time = time.time()
 
-session = analysis.Session(os.path.dirname(base_path))
+session = analysis.Session(os.path.dirname(data_path))
 
 record_node = session.recordnodes[0]
 
 print(fmt_str.format('Total number of recordings', '', str(len(record_node.recordings))))
 
 #print sample rate
-print(fmt_str.format('Sample rate', '', str(record_node.recordings[0].continuous[0].metadata['sample_rate'])))
+sample_rate = record_node.recordings[0].continuous[0].metadata['sample_rate'] if len(continuous.keys()) > 0 else 30000
+
+print(fmt_str.format('Sample rate', '', str(sample_rate)))
 
 recording = record_node.recordings[0]
 
 #Too slow takes 100 seconds to load the whole 0.82GB file
 #print(fmt_str.format('Number of samples per file', '', str(recording.continuous[0].samples.shape[0])))
 
-recording_time = float(samples_per_chan/recording.continuous[0].metadata['sample_rate'])
+recording_time = float(samples_per_chan/sample_rate)
 
 #Print total recording time in seconds
 print(fmt_str.format('Total recording time (seconds)', '', str(round(recording_time,2))))
 
 #Print total recording time in minutes
-#print(fmt_str.format('Total recording time (minutes)', '', str(round(recording_time/60,2))))
+print(fmt_str.format('Total recording time (minutes)', '', str(round(recording_time/60,2))))
 
 
 #Load the first 30 seconds of data
 duration = 120
-sample_rate = int(recording.continuous[0].metadata['sample_rate'])
 end_sample_index = duration*sample_rate
 # TODO: Way slower than loading .dat file directly, why?
 #data = recording.continuous[0].get_samples(start_sample_index=0, end_sample_index=end_sample_index)
 
-first_timestamp = recording.continuous[0].timestamps[0]
-print(fmt_str.format('First timestamp', '', str(first_timestamp)))
+if len(continuous.keys()) > 0:
+    first_sample_number = recording.continuous[0].sample_numbers[0]
+    first_timestamp = recording.continuous[0].timestamps[0]
+else:
+    #Get first timestamp from the first word in second line of messages.events file
+    with open(os.path.join(base_path, 'messages.events'), 'r') as f:
+        f.readline()
+        first_sample_number = int(f.readline().split(' ')[0][:-1])
+    first_timestamp = first_sample_number/sample_rate
 
-first_sample_number = recording.continuous[0].sample_numbers[0]
-#print(fmt_str.format('First sample index', '', str(first_sample_number)))
+print(fmt_str.format('First timestamp', '', str(first_timestamp)))
+print(fmt_str.format('First sample number', '', str(first_sample_number)))
 
 print('-' * fmt_str_len)
 
@@ -129,7 +145,7 @@ print(fmt_str.format('First event', '', str(events.sample_number[0])))
 #Load settings.xml (TODO: add this helper to python-tools)
 import xml.etree.ElementTree as ET
 
-tree = ET.parse(os.path.join(base_path, 'settings.xml'))
+tree = ET.parse(os.path.join(data_path, 'settings.xml'))
 root = tree.getroot()
 
 #Get channel map        
@@ -152,7 +168,7 @@ print(fmt_str.format('Channel ordering after channel map: ', '', str(channel_ord
 print(fmt_str.format('Number of detected events: ', '', str(len(events))))
 
 #Load manually identified ripple events
-text_file = open(os.path.join(base_path, os.path.join('events', 'events_selected_manually.txt')), "r")
+text_file = open(os.path.join(data_path, os.path.join('events', 'events_selected_manually.txt')), "r")
 lines = text_file.readlines()[3:]
 text_file.close()
 events_selected_manually = []
@@ -210,65 +226,67 @@ print(fmt_str.format('True positive rate: ', '', str(round(len(detected_events_w
 print(fmt_str.format('False positive rate: ', '', str(round(1-len(detected_events_within_manual_events.keys())/len(events_selected_manually),2))))
 print(fmt_str.format('Elapsed time (seconds)', '', str(round(float(time.time() - start_time),2))))
 
-PLOT_SUMMARY = False
-if PLOT_SUMMARY:
-    down_sample_factor = 10000
-    samples = samples + np.arange(samples.shape[1]) * 500
-    plt.plot(samples[::down_sample_factor,cnn_input_channels],color='0.5')
-    if events is not None:
-        [plt.axvline(event_sample_number/down_sample_factor, 
-                     color='b', linestyle='-', linewidth=1) for event_sample_number in events.sample_number]
-    if events_selected_manually is not None:
-        [plt.axvline(int(event_sample_number/down_sample_factor),
-                     color='r', linestyle='-', linewidth=1) for event_sample_number in [ int(np.mean([a,b])*sample_rate) for a,b in events_selected_manually]]
-    if detected_events_within_manual_events is not None:
-        [plt.axvline(int(sample_rate*event_sample_number)/down_sample_factor,
-                     color='g', linestyle='-', linewidth=1) for event_sample_number in detected_events_within_manual_events]
-    if detected_events_within_pred_threshold is not None:
-        [plt.axvline(int(sample_rate*event_sample_number)/down_sample_factor,
-                     color='g', linestyle='-', linewidth=1) for event_sample_number in detected_events_within_pred_threshold]
-    plt.axis('off')
-    plt.show()
+PLOT = False
+if PLOT:
+    PLOT_SUMMARY = True
+    if PLOT_SUMMARY:
+        down_sample_factor = 10000
+        samples = samples + np.arange(samples.shape[1]) * 500
+        plt.plot(samples[::down_sample_factor,cnn_input_channels],color='0.5')
+        if events is not None:
+            [plt.axvline(event_sample_number/down_sample_factor, 
+                        color='b', linestyle='-', linewidth=1) for event_sample_number in events.sample_number]
+        if events_selected_manually is not None:
+            [plt.axvline(int(event_sample_number/down_sample_factor),
+                        color='r', linestyle='-', linewidth=1) for event_sample_number in [ int(np.mean([a,b])*sample_rate) for a,b in events_selected_manually]]
+        if detected_events_within_manual_events is not None:
+            [plt.axvline(int(sample_rate*event_sample_number)/down_sample_factor,
+                        color='g', linestyle='-', linewidth=1) for event_sample_number in detected_events_within_manual_events]
+        if detected_events_within_pred_threshold is not None:
+            [plt.axvline(int(sample_rate*event_sample_number)/down_sample_factor,
+                        color='g', linestyle='-', linewidth=1) for event_sample_number in detected_events_within_pred_threshold]
+        plt.axis('off')
+        plt.show()
 
-PLOT_FIRST_N_GROUND_TRUTH = False
-if PLOT_FIRST_N_GROUND_TRUTH:
-    N = 16
-    window_size_in_ms = 150
-    window_size_in_samples = int(window_size_in_ms * sample_rate / 1000 / 2)
-    down_sample_factor = 1
-    samples = samples + np.arange(samples.shape[1]) * 150
-    #Create a sublplot for each event such that the resulting figure has roughly square plots
-    num_rows = int(np.ceil(np.sqrt(N)))
-    num_cols = int(np.ceil(N/num_rows))
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(num_cols*3,num_rows*3))
-    gt_events = events_selected_manually
-    for idx, ax in enumerate(axes.flatten()):
-        event_sample_number = int(sample_rate*np.mean(gt_events[idx]))
-        ax.plot(samples[event_sample_number-window_size_in_samples:event_sample_number+window_size_in_samples:down_sample_factor,cnn_input_channels], color='royalblue')
-        x1 = int((gt_events[idx][0] - event_sample_number/sample_rate) * sample_rate / down_sample_factor) + window_size_in_samples
-        x2 = int((gt_events[idx][1] - event_sample_number/sample_rate) * sample_rate / down_sample_factor) + window_size_in_samples
-        ax.axvspan(x1, x2, facecolor='lightgray', alpha=0.5)
-        ax.axis('off')
-    plt.show()
+    PLOT_FIRST_N_GROUND_TRUTH = False
+    if PLOT_FIRST_N_GROUND_TRUTH:
+        N = 16
+        window_size_in_ms = 150
+        window_size_in_samples = int(window_size_in_ms * sample_rate / 1000 / 2)
+        down_sample_factor = 1
+        samples = samples + np.arange(samples.shape[1]) * 150
+        #Create a sublplot for each event such that the resulting figure has roughly square plots
+        num_rows = int(np.ceil(np.sqrt(N)))
+        num_cols = int(np.ceil(N/num_rows))
+        fig, axes = plt.subplots(num_rows, num_cols, figsize=(num_cols*3,num_rows*3))
+        gt_events = events_selected_manually
+        for idx, ax in enumerate(axes.flatten()):
+            event_sample_number = int(sample_rate*np.mean(gt_events[idx]))
+            ax.plot(samples[event_sample_number-window_size_in_samples:event_sample_number+window_size_in_samples:down_sample_factor,cnn_input_channels], color='royalblue')
+            x1 = int((gt_events[idx][0] - event_sample_number/sample_rate) * sample_rate / down_sample_factor) + window_size_in_samples
+            x2 = int((gt_events[idx][1] - event_sample_number/sample_rate) * sample_rate / down_sample_factor) + window_size_in_samples
+            ax.axvspan(x1, x2, facecolor='lightgray', alpha=0.5)
+            ax.axis('off')
+        plt.show()
 
-PLOT_FIRST_N_GROUND_TRUTH_AND_DETECTED = True
-if PLOT_FIRST_N_GROUND_TRUTH_AND_DETECTED:
-    N = 32
-    window_size_in_ms = 120
-    window_size_in_samples = int(window_size_in_ms * sample_rate / 1000 / 2)
-    down_sample_factor = 1
-    samples = samples + np.arange(samples.shape[1]) * 300
-    #Create a sublplot for each event such that the resulting figure has roughly square plots
-    num_rows = int(np.ceil(np.sqrt(N)))
-    num_cols = int(np.ceil(N/num_rows))
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(num_cols*3,num_rows*3))
-    gt_events = list(detected_events_within_manual_events.keys())
-    for idx, ax in enumerate(axes.flatten()):
-        event_sample_number = int(sample_rate*gt_events[idx])
-        ax.plot(samples[event_sample_number-window_size_in_samples:event_sample_number+window_size_in_samples:down_sample_factor,cnn_input_channels], color='royalblue')
-        ax.axvline(window_size_in_samples, color='k', linestyle='-', linewidth=2)
-        x1 = int((detected_events_within_manual_events[gt_events[idx]][0] - event_sample_number/sample_rate) * sample_rate / down_sample_factor) + window_size_in_samples
-        x2 = int((detected_events_within_manual_events[gt_events[idx]][1] - event_sample_number/sample_rate) * sample_rate / down_sample_factor) + window_size_in_samples
-        ax.axvspan(x1, x2, facecolor='lightgray', alpha=0.5)
-        ax.axis('off')
-    plt.show()
+    PLOT_FIRST_N_GROUND_TRUTH_AND_DETECTED = True
+    if PLOT_FIRST_N_GROUND_TRUTH_AND_DETECTED:
+        N = 32
+        window_size_in_ms = 120
+        window_size_in_samples = int(window_size_in_ms * sample_rate / 1000 / 2)
+        down_sample_factor = 1
+        samples = samples + np.arange(samples.shape[1]) * 300
+        #Create a sublplot for each event such that the resulting figure has roughly square plots
+        num_rows = int(np.ceil(np.sqrt(N)))
+        num_cols = int(np.ceil(N/num_rows))
+        fig, axes = plt.subplots(num_rows, num_cols, figsize=(num_cols*3,num_rows*3))
+        gt_events = list(detected_events_within_manual_events.keys())
+        for idx, ax in enumerate(axes.flatten()):
+            event_sample_number = int(sample_rate*gt_events[idx])
+            ax.plot(samples[event_sample_number-window_size_in_samples:event_sample_number+window_size_in_samples:down_sample_factor,cnn_input_channels], color='royalblue')
+            ax.axvline(window_size_in_samples, color='k', linestyle='-', linewidth=2)
+            x1 = int((detected_events_within_manual_events[gt_events[idx]][0] - event_sample_number/sample_rate) * sample_rate / down_sample_factor) + window_size_in_samples
+            x2 = int((detected_events_within_manual_events[gt_events[idx]][1] - event_sample_number/sample_rate) * sample_rate / down_sample_factor) + window_size_in_samples
+            ax.axvspan(x1, x2, facecolor='lightgray', alpha=0.5)
+            ax.axis('off')
+        plt.show()
