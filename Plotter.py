ad8ac4c2b706b0db367df16ed7fbdbb5e4389062
Pavel Kulik <pavelkulik@hotmail.com>
2023-05-01 14:17:08 -0700

    Add Plotter class

--- Plotter.py
+++ Plotter.py
@@ -2,26 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Sample data
-num_datasets = 8
-num_points = 100
-amplitude = 5
+import math
 
-datasets = [amplitude * np.sin(np.linspace(0, 2 * np.pi, num_points)) + i * 2 * amplitude for i in range(num_datasets)]
+from matplotlib import gridspec
+class RipplePlot:
 
-# Create 3D plot
+    def __init__(self, axes_handle):
+        self.ax = axes_handle
+        self.num_datasets = 8
+        self.num_points = 100
+        self.amplitude = 5
+
+        datasets = [self.amplitude * np.sin(np.linspace(0, 2 * np.pi, self.num_points)) + i * 2 * self.amplitude for i in range(self.num_datasets)]
+
+        # Plot each dataset along the Z-axis
+        for i, data in enumerate(datasets):
+            x = np.arange(self.num_points)
+            y = np.ones(self.num_points) * i
+            z = data
+            self.ax.plot(x, y, z)
+            self.ax.axis('off')
+
+N = 8
+cols = 4
+rows = int(math.ceil(N / cols))
+
+gs = gridspec.GridSpec(rows, cols)
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+for n in range(N):
+    ax = fig.add_subplot(gs[n], projection='3d')
+    RipplePlot(ax)
 
-# Plot each dataset along the Z-axis
-for i, data in enumerate(datasets):
-    x = np.arange(num_points)
-    y = np.ones(num_points) * i
-    z = data
-    ax.plot(x, y, z)
-    ax.axis('off')
-
-
-
-# Show the plot
+fig.tight_layout()
 plt.show()
